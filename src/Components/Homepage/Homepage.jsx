fbfa6bb98e6d065fe5f3161aaa1d0c334d17c64a
Muzammil-khan710 <khanmuzammil549@gmail.com>
2022-03-29 14:03:57 +0530

    Feat: Homepage text

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -6,8 +6,9 @@ const Homepage = () => {
   return (
     <div className='image-container homepage-container'>
         <img style={{height:"auto",width:"100%"}} src={HomeImg} alt="" />
-        <section>
-            <button>Explore all categories</button>
+        <section className='section-area'>
+            <h2 className='section-text'>Exclusive offer upto 50% off</h2>
+            <button className='explore-btn'>Explore all categories</button>
         </section>
     </div>
   )
