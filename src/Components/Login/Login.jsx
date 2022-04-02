01758fede67d5aa654607e157624ac2070921ba4
Muzammil-khan710 <khanmuzammil549@gmail.com>
2022-04-02 17:06:27 +0530

    Fixed: added form tag and submit button

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,15 +5,18 @@ import "./Login.css"
 const LoginPage = () => {
   return (
     <div>
-        <h1>Hello, Welcome again</h1>
+      <h1>Hello, Welcome again</h1>
       <div className="login-container">
         <div className="login-content">
           <h2>Log in</h2>
+          <form>
           <p>Enter your email</p>
           <input className="input" type="text" placeholder="name@mail.com"/>
           <p>Enter your password</p>
           <input className="input" type="password" placeholder="****" />
+          <button className='btn d-grey btn-auth' >Submit</button>
           <p className="t-cntr">or</p>
+          </form>
           <p>New here? <Link className=" link sign-up-link" to="/Signup">Sign-up</Link></p>
         </div>
       </div>
--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -9,12 +9,15 @@ const SignupPage = () => {
         <div className="signup-container">
         <div className="login-content">
           <h2>Sign up</h2>
+          <form>
           <p>Enter your name</p>
           <input className="input" placeholder="Name" />  
           <p>Enter your email</p>
           <input className="input" type="text" placeholder="name@mail.com"/>
           <p>Enter your password</p>
           <input className="input" type="password" placeholder="****" />
+          <button className='btn d-grey btn-auth'>Submit</button>
+          </form>
           <p className="t-cntr">or</p>
           <p>Already a customer? <Link className="link login-link" to="/Login">Log in</Link></p>
         </div>
