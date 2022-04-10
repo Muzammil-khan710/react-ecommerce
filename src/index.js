fecc35b49e62288c883d5bb351f12ac22112a3bf
Muzammil-khan710 <khanmuzammil549@gmail.com>
2022-04-10 18:34:40 +0530

    Feat: Wishlist page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { CartProvider } from "./context/Cart-context";
+import { WishlistProvider } from "./context/Wishlist-context";
 
 // Call make Server
 makeServer();
@@ -12,9 +13,11 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <WishlistProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </WishlistProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
